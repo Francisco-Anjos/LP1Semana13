3cca97c83978ad1631d3141eacbddfbe0e4323e2
nightwolf anjels <fcariaanjos@gmail.com>
2023-06-08 23:27:09 +0100

    finished SuperList

--- MultipleStuff/SuperLIst.cs
+++ MultipleStuff/SuperLIst.cs
@@ -5,6 +5,72 @@ public class SuperList : List<double>
 {
     public (double Min, double Max) GetMinMax1()
     {
+        double min = double.MaxValue;
+        double max = double.MinValue;
 
+        foreach (double number in this)
+        {
+            if (number < min)
+                min = number;
+
+            if (number > max)
+                max = number;
+        }
+
+        return (min, max);
+    }
+
+    public (double Min, double Max) GetMinMax2()
+    {
+        MinMaxPair pair = new MinMaxPair();
+        foreach (double number in this)
+        {
+            if (number < pair.Min)
+                pair.Min = number;
+
+            if (number > pair.Max)
+                pair.Max = number;
+        }
+
+        return (pair.Min, pair.Max);
+    }
+       public (double Min, double Max) GetMinMax3()
+    {
+        double min = double.MaxValue;
+        double max = double.MinValue;
+
+        foreach (double number in this)
+        {
+            if (number < min)
+                min = number;
+
+            if (number > max)
+                max = number;
+        }
+
+        return (min, max);
+    }
+
+    public (double Minimum, double Maximum) GetMinMax4()
+    {
+        double minimum = double.MaxValue;
+        double maximum = double.MinValue;
+
+        foreach (double number in this)
+        {
+            if (number < minimum)
+                minimum = number;
+
+            if (number > maximum)
+                maximum = number;
+        }
+
+        return (minimum, maximum);
+    }
+
+    private class MinMaxPair
+    {
+        public double Min { get; set; } = double.MaxValue;
+        public double Max { get; set; } = double.MinValue;
     }
 }
