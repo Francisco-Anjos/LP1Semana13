29a844b2cb05fb258343d52070a32aabd57e3595
nightwolf anjels <fcariaanjos@gmail.com>
2023-06-08 23:31:28 +0100

    finished palindrome

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace Palindrome
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        foreach (string arg in args)
         {
-            Console.WriteLine("Hello, World!");
+            bool isPalindrome = IsPalindrome(arg);
+            Console.WriteLine($"{arg} -> {isPalindrome}");
         }
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        if (str == null)
+            throw new ArgumentNullException(nameof(str));
+
+        if (str.Length < 2)
+            return true;
+
+        return IsPalindromeRecursive(str, 0, str.Length - 1);
+    }
+
+    static bool IsPalindromeRecursive(string str, int start, int end)
+    {
+        if (start >= end)
+            return true;
+
+        if (str[start] != str[end])
+            return false;
+
+        return IsPalindromeRecursive(str, start + 1, end - 1);
     }
 }
